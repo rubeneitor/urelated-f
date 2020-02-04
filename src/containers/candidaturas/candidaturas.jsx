cc8954465a6065c25ed0058ddaa606531f6677a5
Dave86 <davidochando1986@gmail.com>
2020-02-04 12:40:40 +0100

    conexion front suscripciones empresa

--- src/containers/candidaturas/candidaturas.jsx
+++ src/containers/candidaturas/candidaturas.jsx
@@ -1,5 +1,8 @@
 
 import React from "react";
+import { session, getUrl} from "../../utils/uti";
+import axios from "axios";
+import "./candidaturas.scss";
 
 
 class Candidaturas extends React.Component {
@@ -7,7 +10,27 @@ class Candidaturas extends React.Component {
     constructor (props) {
         super(props);
         
+        this.state = {
+            userType : session.get()?.userType,
+        }
     };
+
+    async componentDidMount(){
+        
+        //comprobamos si se trata de una empresa o un candidato
+        if(this.state.userType === "Empresa"){
+            //buscamos las candidaturas por empresa
+            const id_empresa = session.get()?.visitor_id;
+
+            //axios...
+            const res = await axios.get(getUrl(`/suscripcionesPorE?id_empresa=${id_empresa}`));
+            console.log(res);
+
+        }else{
+            //buscamos las candidaturas por candidato
+        }
+
+    }
     
     render() {
         return(
