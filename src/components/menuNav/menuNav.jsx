cf2593d79d26ae8255eac9daec7fb9a161e624c4
Dave86 <davidochando1986@gmail.com>
2020-02-04 11:45:25 +0100

    Suscripciones por empresa tooltip link fixed

--- src/components/menuNav/menuNav.jsx
+++ src/components/menuNav/menuNav.jsx
@@ -60,7 +60,7 @@ class Menunav extends React.Component {
                             <li className="dd-list-item-head">{profileName}</li>
                             <li onClick={() => {this.redirect(`profileC?id=${id_visitor}&name=${profileName}`)}} className="dd-list-item">Perfil</li>
                             <li onClick={() => {this.redirect(`curriculum?id=${id_visitor}&name=${profileName}`)}} className="dd-list-item">Currículum</li>
-                            <li onClick={() => {this.redirect("candidaturas")}} className="dd-list-item">Candidaturas</li>
+                            <li onClick={() => {this.redirect(`candidaturas?id=${id_visitor}&name=${profileName}`)}} className="dd-list-item">Candidaturas</li>
                             <li onClick={() => {this.pulsaLogout()}} className="dd-list-item">Log out</li>
                         </ul>
                     </div>
@@ -74,7 +74,7 @@ class Menunav extends React.Component {
                             <li className="dd-list-item-head">{profileName}</li>
                             <li onClick={() => {this.redirect(`profileE?id=${id_visitor}&name=${profileName}`)}} className="dd-list-item2">Perfil</li>
                             <li onClick={() => {this.redirect("ofertas")}} className="dd-list-item2">Ofertas</li>
-                            <li onClick={() => {this.redirect("candidaturas")}} className="dd-list-item2">Candidaturas</li>
+                            <li onClick={() => {this.redirect(`candidaturas?id=${id_visitor}&name=${profileName}`)}} className="dd-list-item2">Candidaturas</li>
                             <li onClick={() => {this.pulsaLogout()}} className="dd-list-item2">Log out</li>
                         </ul>
                     </div>
